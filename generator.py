ff017189db508eb4deaec0449fb850b4bca7aec7
ivanovilya <ivanov.ilya.rus@gmail.com>
2016-07-25 22:21:13 +0400

    add new generator

--- generator.py
+++ generator.py
@@ -1,3 +1,166 @@
+class MaxPushdownGeneratorSpecialCase(object):
+    def __init__(self, n, fileName):
+        self.sim_n = n
+        self.n = 11 * n
+        self.m = 2
+        self.k = 2
+        
+        with open(fileName, 'w') as fout:
+            fout.write(str(self.n))
+            fout.write('\t')
+            fout.write(str(self.m))
+            fout.write('\t')
+            fout.write(str(self.k))
+            fout.write('\n')
+            fout.write('0')
+            fout.write('\t\n')
+            
+            fout.write('phi\n')
+            for q in range(self.n):
+                for z in range(self.m+1):
+                    fout.write(str(q) + '\t' + str(z) + '\t' + str(self.phi(q,z)) + '\n')
+ 
+            fout.write('psi\n')
+            for q in range(self.n):
+                for z in range(self.m+1):
+                    fout.write(str(q) + '\t' + str(z) + '\t' + str(self.psi(q,z)) + '\n')
+            
+            fout.write('eta\n')
+            for q in range(self.n):
+                for z in range(self.m+1):
+                    fout.write(str(q) + '\t' + str(z) + '\t' + str(self.eta(q,z)) + '\n')
+                    
+    def psi(self, q, z):
+        if q == 0 and z == 0:
+            return 1
+        return 0
+    
+    def phi(self, q, z):
+        num, level = getNumAndLevel(q)
+        
+        if level > 0 and num == 2 and z == 2:
+            return getState(0, level - 1)
+        
+        if level < self.sim_n - 1 and num == 10:
+            return getState(0, level + 1)
+            
+        if num == 0 and z == 1:
+            return getState(1, level)
+        
+        if num == 0 and z == 2:
+            return getState(2, level)
+        
+        if num == 0 and z == 0:
+            return getState(3, level)
+        
+        if num == 3:
+            return getState(4, level)
+            
+        if num == 4:
+            return getState(5, level)
+        
+        if num == 1 and z == 1:
+            return getState(5, level)
+        
+        if num == 5:
+            return getState(6, level)
+            
+        if num == 6:
+            return getState(7, level)
+
+            
+        if num == 2 and z == 1:
+            return getState(7, level)
+        
+        if num == 7:
+            return getState(8, level)
+        
+        if num == 8:
+            return getState(9, level)
+            
+        if num == 1 and level < self.sim_n - 1 and z == 2:
+            return getState(9, level)
+            
+        if num == 9 and level < self.sim_n - 1:
+            return getState(10, level)
+        
+        if num == 1 and level == self.sim_n - 1 and z == 2:
+            return getState(0, level)
+
+        if num == 9 and level == self.sim_n - 1:
+            return getState(0, level)
+        
+        return q
+        
+                    
+    def eta(self, q, z):
+        num, level = getNumAndLevel(q)
+        
+        if level > 0 and num == 2 and z == 2:
+            return '0'
+        
+        if level < self.sim_n - 1 and num == 10:
+            return '11'
+            
+        if num == 0 and z == 1:
+            return '0'
+        
+        if num == 0 and z == 2:
+            return '0'
+        
+        if num == 0 and z == 0:
+            return '11'
+
+        if num == 3:
+            return '11'
+            
+        if num == 4:
+            return '11'
+        
+        if num == 1 and z == 1:
+            return '11'
+        
+        if num == 5:
+            return '21'
+            
+        if num == 6:
+            return '21'
+
+        if num == 2 and z == 1:
+            return '21'
+        
+        if num == 7:
+            return '11'
+        
+        if num == 8:
+            return '21'
+            
+        if num == 1 and level < self.sim_n - 1 and z == 2:
+            return '21'
+            
+        if num == 9 and level < self.sim_n - 1:
+            return '21'
+        
+        if num == 1 and level == self.sim_n - 1 and z == 2:
+            return '0'
+
+        if num == 9 and level == self.sim_n - 1:
+            return '1'
+        
+        return '0'
+    
+    def theoryEstimate(self):
+        return 15*4**self.sim_n - 19
+
+def getState(num, level):
+    return num + 11 * level
+    
+def getNumAndLevel(q):
+    num = q % 11
+    level = q / 11
+    return num, level
+
+
 class MaxPushdownGenerator(object):
     def __init__(self, n, m, k, fileName):
         self.n = n
